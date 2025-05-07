a721de0dcf89786d2f64c13033dfc3f200d346a1
Nepo Rojas • GECI <nepo.rojas@gmail.com>
2025-05-07 10:22:41 -0700

    ✨ Update description

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -98,8 +98,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Dirección de Análisis de Procesos e Informática',
+        text: "Somos el puente entre los sistemas y la estrategia: usamos métodos cuantitativos para resolver problemas gubernamentales, y herramientas económicas para garantizar su viabilidad. De la automatización a la inteligencia artificial, nuestro enfoque interdisciplinario impulsa la eficiencia en toda la organización.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
